102db0b30f47957dd1ca46ea145596e56074cb53
Sean Davitt <sean.davitt@tylertech.com>
2021-05-20 00:45:50 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,324 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace ChatTextImport
+{
+    internal class Program
+    {
+        //main entrance to application
+        private static void Main(string[] args)
+        {
+            string LogEntry = @"----------- Start of log file" + " " + DateTime.Now + "-----------";
+
+            Program program = new Program();
+
+            //this will put the current date of application start per run. This allows for easier readability.
+            if (File.Exists("ChatTextImport Log.txt"))
+            {
+                using (StreamWriter file = new StreamWriter(("ChatTextImport Log.txt"), true))
+                {
+                    file.WriteLine(LogEntry);
+                    Task Task4 = Task.Factory.StartNew(() => program.LogEntryWriter(LogEntry));
+                }
+            }
+            else
+            {
+                using (StreamWriter file = new StreamWriter(("ChatTextImport Log.txt"), true))
+                {
+                    file.WriteLine(LogEntry);
+                    program.LogEntryWriter(LogEntry);
+                }
+            }
+
+            program.SearchFolder(@"E:\Recordings\TwitchChatIngest");
+
+            program.MoveFiles(@"E:\Recordings\TwitchChatIngest\", @"E:\Recordings\TwitchChatDelete\");
+            program.DeleteFiles(@"E:\Recordings\TwitchChatDelete\");
+        }
+
+        //searches through the predefined folder for files that are not empty
+        public void SearchFolder(string location)
+        {
+            foreach (var file in Directory.GetFiles(location))
+            {
+                if (new FileInfo(file).Length == 0)
+                {
+                    Task Task4 = Task.Factory.StartNew(() => MoveFiles(Path.GetDirectoryName(file), @"E:\Recordings\TwitchChatDelete\"));
+
+                    string LogEntry = DateTime.Now + " " + file + " was moved because it was empty";
+
+                    LogEntryWriter(LogEntry);
+
+                    Task.WaitAll(Task4);
+                }
+                else
+                {
+                    SearchFileForName(file);
+                }
+            }
+        }
+
+        //searches through files for message specific user data
+        private void SearchFileForMessage(string File)
+        {
+            string filePath = Path.Combine(Path.GetDirectoryName(File), Path.GetFileName(File));
+            int lineNumber = 1;
+            try
+            {
+                using (StreamReader inputFile = new StreamReader(filePath))
+                {
+                    for (int i = 1; i <= lineNumber; i++)
+                    {
+                        string message = inputFile.ReadLine();
+
+                        return;
+                    }
+                }
+            }
+            catch
+            {
+                SearchFileForMessage(File);
+            }
+
+            Console.ReadLine();
+        }
+
+        //searches through files for data on the db data
+        private void SearchFileForName(string File)
+        {
+            string filePath = Path.Combine(Path.GetDirectoryName(File), Path.GetFileName(File));
+
+            try
+            {
+                if (new FileInfo(filePath).Length == 0)
+                {
+                }
+                else
+                {
+                    using (StreamReader inputFile = new StreamReader(filePath))
+                    {
+                        string message = inputFile.ReadLine().TrimStart('U', 'M', ':', ' ');
+                        string name = inputFile.ReadLine().TrimStart('U', 'N', ':', ' ');
+                        string mod = inputFile.ReadLine().TrimStart('I', 'M', ':', ' ');
+                        string sub = inputFile.ReadLine().TrimStart('I', 'S', ':', ' ');
+                        string SubTime = inputFile.ReadLine().TrimStart('S', 'L', ':', ' ', '/');
+
+                        Task Task1 = Task.Factory.StartNew(() => InsertName(name, mod, sub, SubTime));
+
+                        Task Task2 = Task.Factory.StartNew(() => InsertMessage(message));
+
+                        Task Task3 = Task.Factory.StartNew(() => InsertNameandMessage(name, message));
+
+                        Task.WaitAll(Task1, Task2, Task3);
+
+                        return;
+                    }
+                }
+            }
+            catch
+            {
+                SearchFileForName(File);
+            }
+        }
+
+        //will search through files for user name and message
+        private void SearchFileForBoth(string File)
+        {
+            string filePath = Path.Combine(Path.GetDirectoryName(File), Path.GetFileName(File));
+            int lineNumber = 1;
+            try
+            {
+                using (StreamReader inputFile = new StreamReader(filePath))
+                {
+                    for (int i = 1; i <= lineNumber; i++)
+                    {
+                        string message = inputFile.ReadLine();
+                        string name = inputFile.ReadLine();
+
+                        return;
+                    }
+                }
+            }
+            catch
+            {
+                SearchFileForBoth(File);
+            }
+
+            Console.ReadLine();
+        }
+
+        //will insert user message into local db
+        private void InsertMessage(string message)
+        {
+            string connetionString;
+            SqlDataReader rdr = null;
+            SqlConnection cnn;
+            connetionString = @"Data Source=LAZERUS\SQLEXPRESS;Initial Catalog=STREAMINGDB;Integrated Security=SSPI;";
+            cnn = new SqlConnection(connetionString);
+            cnn.Open();
+
+            try
+            {
+                SqlCommand cmd = new SqlCommand("InsertUserMessage", cnn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add(new SqlParameter("@UserMessage", message));
+
+                rdr = cmd.ExecuteReader();
+                rdr.Close();
+            }
+            catch (Exception e)
+            {
+                if (string.IsNullOrEmpty(message))
+                {
+                    string Message = "BLANK VALUE";
+                    SqlCommand cmd = new SqlCommand("InsertUserMessage", cnn);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.Add(new SqlParameter("@UserMessage", Message));
+
+                    rdr = cmd.ExecuteReader();
+                    rdr.Close();
+                }
+                else
+                {
+                    string LogEntry1 = DateTime.Now + e.ToString();
+
+                    LogEntryWriter(LogEntry1);
+                }
+            }
+
+            cnn.Close();
+        }
+
+        //will insert user name into local db
+        private void InsertName(string Name, string Mod, string Sub, string SubTime)
+        {
+            string connetionString;
+            SqlDataReader rdr = null;
+            SqlConnection cnn;
+            connetionString = @"Data Source=LAZERUS\SQLEXPRESS;Initial Catalog=STREAMINGDB;Integrated Security=SSPI;";
+            cnn = new SqlConnection(connetionString);
+            cnn.Open();
+
+            try
+            {
+                SqlCommand cmd = new SqlCommand("InsertUserName", cnn);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                List<SqlParameter> prm = new List<SqlParameter>()
+                {
+                     new SqlParameter("@UserName", SqlDbType.NVarChar) {Value = Name},
+                     new SqlParameter("@Is_Subscribed", SqlDbType.NVarChar) {Value = Sub},
+                     new SqlParameter("@Sub_Length", SqlDbType.Int) {Value = SubTime},
+                     new SqlParameter("@Is_Moderator", SqlDbType.NVarChar) {Value = Mod},
+                };
+                cmd.Parameters.AddRange(prm.ToArray());
+
+                rdr = cmd.ExecuteReader();
+
+                rdr.Close();
+            }
+            catch (Exception e)
+            {
+                string LogEntry1 = DateTime.Now + e.ToString();
+
+                LogEntryWriter(LogEntry1);
+            }
+
+            cnn.Close();
+        }
+
+        //will insert user name and message into local db
+        private void InsertNameandMessage(string Name, string Message)
+        {
+            string connetionString;
+            SqlDataReader rdr = null;
+            SqlConnection cnn;
+            connetionString = @"Data Source=LAZERUS\SQLEXPRESS;Initial Catalog=STREAMINGDB;Integrated Security=SSPI;";
+            cnn = new SqlConnection(connetionString);
+            cnn.Open();
+
+            try
+            {
+                SqlCommand cmd = new SqlCommand("InsertUserNameandMessage", cnn);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                List<SqlParameter> prm = new List<SqlParameter>()
+                {
+                     new SqlParameter("@UserName", SqlDbType.NVarChar) {Value = Name},
+                     new SqlParameter("@UserMessage", SqlDbType.NVarChar) {Value = Message},
+                     //new SqlParameter("@variable3", SqlDbType.DateTime) {Value = myValue3},
+                };
+                cmd.Parameters.AddRange(prm.ToArray());
+
+                rdr = cmd.ExecuteReader();
+
+                rdr.Close();
+            }
+            catch (Exception e)
+            {
+                string LogEntry1 = DateTime.Now + e.ToString();
+
+                LogEntryWriter(LogEntry1);
+            }
+
+            cnn.Close();
+        }
+
+        //will move files from one folder to another
+        private void MoveFiles(string Start, string End)
+        {
+            foreach (var file in Directory.GetFiles(Start))
+            {
+                if (new FileInfo(file).Length == 0)
+                {
+                    MoveFile(Start, End);
+                }
+                else
+                {
+                    File.Move(Path.Combine(Start, Path.GetFileName(file)), Path.Combine(End, Path.GetFileName(file)));
+                }
+            }
+        }
+
+        //will move a file if it is blank
+        private void MoveFile(string Start, string End)
+        {
+            foreach (var file in Directory.GetFiles(Start))
+            {
+                if (new FileInfo(file).Length == 0)
+                {
+                    File.Move(Path.Combine(Start, Path.GetFileName(file)), Path.Combine(End, Path.GetFileName(file)));
+                }
+            }
+        }
+
+        //delete all files found in a folder
+        private void DeleteFiles(string location)
+        {
+            foreach (var file in Directory.GetFiles(location))
+            {
+                File.Delete(Path.Combine(location, file));
+            }
+        }
+
+        //This will write to a log file to keep between runs
+        private void LogEntryWriter(string LogEntry)
+        {
+            try
+            {
+                using (TextWriter text_writer = new StreamWriter(("ChatTextImport Log.txt"), true))
+                {
+                    text_writer.WriteLine(LogEntry);
+                }
+            }
+            catch (Exception e)
+            {
+                LogEntryWriter(LogEntry);
+            }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ChatTextImport")]
+[assembly: AssemblyDescription("Designed to parse and export text from file generated from Twitch Chat")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Davasorus")]
+[assembly: AssemblyProduct("ChatTextImport")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("Davasorus")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(true)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e912c847-4a14-4be5-a8a9-ecac15b3dbe1")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("21.1.4.2")]
